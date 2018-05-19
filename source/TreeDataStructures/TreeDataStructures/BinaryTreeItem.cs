3118106d59bdd251387b35e818d112c75203d070
stargateKing <mmujagic@gmail.com>
2018-05-18 21:48:22 -0400

    Adding binary tree

---
+++ source/TreeDataStructures/TreeDataStructures/BinaryTreeItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeDataStructures
+{
+    public class BinaryTreeItem<T> : ITreeItem<T>
+    {
+        private ITreeItem<T>[] _children;
+
+        public BinaryTreeItem(T value)
+        {
+            this.Item = value;
+            this._children = new ITreeItem<T>[2];
+        }
+
+        public T Item { get; set; }
+
+        public IEnumerable<ITreeItem<T>> Children => this._children;
+
+        public ITreeItem<T> LeftChild {
+            get
+            {
+                return this._children[0];
+            }
+            set
+            {
+                this._children[0] = value;
+            }
+        }
+
+        public ITreeItem<T> RightChild
+        {
+            get
+            {
+                return this._children[1];
+            }
+            set
+            {
+                this._children[1] = value;
+            }
+        }
+    }
+}
---
+++ source/TreeDataStructures/TreeDataStructures/ITreeItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeDataStructures
+{
+    public interface ITreeItem<T>
+    {
+        /// <summary>
+        /// The getter and setter for the item of specified type.
+        /// </summary>
+        T Item { get; set; }
+
+        /// <summary>
+        /// A getter for the children.
+        /// </summary>
+        IEnumerable<ITreeItem<T>> Children { get; }
+    }
+}
